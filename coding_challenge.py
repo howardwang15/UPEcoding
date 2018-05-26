b3d6d0a48b0a6fc9ba70fec96cb813cc311b9eeb
Howard Wang <joy_luck_2000@hotmail.com>
2018-05-26 07:12:27 -0700

    Finished the coding challenge
    Finally finished the hangman...after an all-nighter :)

--- coding_challenge.py
+++ coding_challenge.py
@@ -6,13 +6,14 @@ import time
 endpoint = 'http://upe.42069.fun/v5J8d'
 words_path = "words.txt"
 
-letters = "ESIARNTOLCDUPMGHBYFVKWZXQJ" #http://datagenetics.com/blog/april12012/index.html
+letters = "ESIARNTOLDUYPMCGHBFKWVZXQJ" #http://datagenetics.com/blog/april12012/index.html
 letters = letters.lower()
 most_popular_letters = list(letters)
 
 def init():
-    file = open(words_path)
+    file = open(words_path, 'r')
     words_list = [line.strip('\n') for line in file]
+    file.close()
     return words_list, file
 
 
@@ -31,6 +32,7 @@ def get_words_same_length(state, words_array):
 
 def find_possible_words(state, possibles):
     if len(possibles) != len(state):
+        print("state: {}".format(state))
         print("state: {}".format(state))
         print("possibles: {}".format(possibles))
         print("state length: {}".format(len(state)))
@@ -54,10 +56,10 @@ def find_possible_words(state, possibles):
 
         if len(letter_dict) == 0:
             possibles_new.append(possibles[i]) #append the old possible words list
-            print("added old words as new words: {}".format(j))
+            #print("added old words as new words: {}".format(possibles[i]))
             continue
 
-        print("length: {}".format(len(possibles)))
+        #print("length: {}".format(len(possibles)))
         for word in possibles[i]: #possibles[i] = all the words that might be at position i of state
             #print("possible word for state word {0} (length {1}): {2} (length {3})".format(state[i], len(state[i]), word, len(word)))
             matching_letters = 0
@@ -69,8 +71,7 @@ def find_possible_words(state, possibles):
             if matching_letters == len(letter_dict): #all letters are in promising positions
                 good_words.append(word)
 
-        if len(good_words) == 0:
-            print("no good words were found")
+
         possibles_new.append(good_words)
 
     return possibles_new
@@ -78,18 +79,25 @@ def find_possible_words(state, possibles):
 
 def get_most_frequent_letter(words, used_letters):
     frequencies = [0]*26
-
+    #print("used in get fnc: {}".format(used_letters))
     for i in range(len(words)):
         for word in words[i]:
+            #print("cur word: {}".format(word))
             for letter in word:
                 already_used = False
                 for used in used_letters:
-                    if letter == used:
+                    if letter == used or letter == '\'':
                         already_used = True
                         break
                 if not already_used:
                     frequencies[ord(letter) - 97] += 1
+    #print(frequencies)
+    if max(frequencies) == 0:
+        for popular_letters in most_popular_letters:
+            if popular_letters not in used_letters:
+                return popular_letters, 0
 
+    #print("getting most frequent letter: {}".format(chr(frequencies.index(max(frequencies)) + 97)))
     return chr(frequencies.index(max(frequencies)) + 97), max(frequencies)
 
 
@@ -98,23 +106,22 @@ def get_remaining_letters(letters_used):
 
 
 def main():
-    rounds_played = 0
 
-    additional_words = []
+    rounds_played = 0
     while rounds_played < 100: #initialization step
         words_list, file = init()
         data = requests.get(endpoint).json()
+
+        #temp = requests.post(endpoint + "/reset", data={'email': 'howardwang15@gmail.com'})
+
+        #print(temp.status_code)
         state = data['state'].split()
         status = data['status']
         remaining_guesses = data['remaining_guesses']
         remaining_guesses_before_last_guess = remaining_guesses
         possible_words = get_words_same_length(state, words_list)
-        for newly_found in additional_words:
-            file.write(newly_found)
-            file.write("\n")
-        file.close()
-        additional_words = []
-        assert (len(possible_words) == len(state))
+
+        assert (len(possible_words) == len(state)) #this should always be true
         for i in range(len(state)):
             for j in range(len(possible_words[i])):
                 # print("state length: {0}, word length: {1}".format(len(state[i]), len(possible_words[i][j])))
@@ -122,46 +129,57 @@ def main():
 
 
         used_letters = []
-        char = 'e'
+        char = 'e' #start guess with 'e'
         hack = False
         while status == 'ALIVE':
             if hack:
                 break
+
             response = requests.post(endpoint, data={'guess': char})
+            #print("char: {}".format(char))
+            used_letters.append(char) #add most recently posted letter
             data = response.json()
             state = data['state'].split()
             status = data['status']
             remaining_guesses = data['remaining_guesses']
-            if remaining_guesses == remaining_guesses_before_last_guess:
-                possible_words = find_possible_words(state, possible_words)
-                if len(possible_words) != len(state) and len(possible_words) != 1:
-                    print("what the actual fuck")
-                if len(possible_words) != 0 and len(possible_words[0]) != 0 and possible_words[0][0] == 'Z':
-                    print("error encountered")
-                    break
+
+            #if remaining_guesses == remaining_guesses_before_last_guess:
+            possible_words = find_possible_words(state, possible_words)
+
+            if len(possible_words) != 0 and len(possible_words[0]) != 0 and possible_words[0][0] == 'Z':
+                #print("error encountered")
+                break
+
             remaining_guesses_before_last_guess = remaining_guesses
-            used_letters.append(char)
+            only_word = False
+            for words in possible_words:
+                if len(words) == 1: #only one word left so be efficient
+                    for letter in words[0]:
+                        if letter not in used_letters:
+                            char = letter
+                            only_word = True
+
+            if only_word:
+                #print("only word")
+                continue
+
             special_case = False
             for words in possible_words:
                 for word in words:
-                    if len(word) == 1 or len(word) == 2 and 'a' not in used_letters: #hack for common 1-2 letter words
+                    if (len(word) == 1 or len(word) == 2) and 'a' not in used_letters: #hack for common 1-2 letter words
                         char = 'a'
                         special_case = True
-                    elif len(word) == 1 or len(word) == 2 and 'i' not in used_letters:
+                    elif (len(word) == 1 or len(word) == 2) and 'i' not in used_letters:
                         char = 'i'
                         special_case = True
+
             if not special_case:
                 char, frequency = get_most_frequent_letter(possible_words, used_letters)
-                if remaining_guesses == 1 and frequency <= 2:
-                    hack = True
 
         rounds_played += 1
-        print("rounds_played: {}".format(rounds_played))
-        if status == 'DEAD':
-            for word in data['lyrics'].split():
-                additional_words.append(word)
+        #print("rounds_played: {}".format(rounds_played))
 
-    print(data['win_rate'])
+    #print(data['win_rate'])
 
 
 if __name__ == '__main__':
